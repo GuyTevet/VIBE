938c7b9f495fd6ea3672187e631baed1c4978698
GuyTevet <guy.tvt@gmail.com>
2021-12-06 13:46:12 +0200

    torch interp - splited to train(demo) and vis scripts

---
+++ torchinterp_demo.py
@@ -0,0 +1,174 @@
+import torch
+from torchinterp1d import Interp1d
+import os
+from tqdm import tqdm
+import pickle
+
+torch.autograd.set_detect_anomaly(True)
+device = 'cpu'
+
+class ValOffsetModel(torch.nn.Module):
+    def __init__(self, model_type, val_params, offset_params):
+        super(ValOffsetModel, self).__init__()
+        assert val_params['min'] < val_params['max']
+        assert offset_params['min'] < offset_params['max']
+        self.val_params = val_params
+        self.offset_params = offset_params
+        self.output_size = val_params['size'] + offset_params['size']
+        self.model_type = model_type
+
+        if self.model_type == 'mlp':
+            width=25
+            self.lin1 = torch.nn.Linear(width, width)
+            self.lin2 = torch.nn.Linear(width, self.output_size)
+        elif self.model_type == 'gru':
+            n_layers = 2
+            bs = 1
+            h_size = 10
+            width = val_params['size']
+            self.gru = torch.nn.GRU(input_size=1, hidden_size=h_size, num_layers=n_layers)
+            self.lin = torch.nn.Linear(h_size, 2)
+            self.h0 = torch.randn(n_layers, bs, h_size).to(device)
+        else:
+            raise ValueError('unsupported model type [{}]'.format(self.model_type))
+
+        self.relu = torch.nn.ReLU(inplace=True)
+        self.sig = lambda x, _min, _max: torch.sigmoid(x) * (_max - _min) + _min
+        self.input_noise = torch.rand(width).to(device)
+        if self.model_type == 'gru':
+            self.input_noise = self.input_noise.reshape(self.input_noise.shape[0], 1, 1)
+            
+
+    def forward(self):
+        if self.model_type == 'mlp':
+            out = self.lin1(self.input_noise)
+            out = self.relu(out)
+            out = self.lin2(out)
+            val = out[:self.val_params['size']]
+            offset = out[self.val_params['size']:]
+        elif self.model_type == 'gru':
+            out, _ = self.gru(self.input_noise, self.h0)  # [n_samples, 1, h_size]
+            # out = torch.zeros([self.input_noise.shape[0], 1, 10]).cuda()
+            # out = torch.squeeze(out)
+            out = self.relu(out)
+            out = self.lin(out.view(-1, out.size(-1)))
+            val = out[: ,0]
+            offset = out[1:-1, 1]
+
+        return {
+            'val': self.sig(val, self.val_params['min'], self.val_params['max']),
+            'offset': self.sig(offset, self.offset_params['min'], self.offset_params['max']),
+        }
+
+
+class Interpolator(torch.nn.Module):
+    def __init__(self, model_type, method, n_samples, amp, min_offset, max_offset):
+        super(Interpolator, self).__init__()
+        self.method = method
+        self.n_samples = n_samples
+        self.model = ValOffsetModel(model_type,
+                                    val_params={'min': -amp,  'max': amp, 'size': n_samples},
+                                    offset_params={'min': min_offset,  'max': max_offset, 'size': n_samples-2})
+        self.interp = Interp1d()
+
+    def forward(self, x_new, x_sampled):
+        res = self.model()
+
+        if self.method == 'no_sampling':
+            return {'y_pred': res['val']}
+        else:
+            y_hat = res['val']
+            offsets = res['offset']
+            offsets = torch.cat((torch.tensor([0.]).to(device), offsets, torch.tensor([0.]).to(device)))
+            if self.method == 'non_adaptive':
+                x_hat = x_sampled
+            elif self.method == 'adaptive':
+                x_hat = x_sampled + offsets
+            elif self.method == 'super_adaptive':
+                offsets[-2] /= 2.  # FIXME - ugly hack for super-adaptive case (avoiding the assertion)
+                x_hat = torch.zeros_like(y_hat)
+                for i in range(1, len(x_sampled) - 1):
+                    x_hat[i] = x_hat[i - 1] + offsets[i]
+                x_hat[-1] = seqlen - 1
+                # assert x_hat[-1] > x_hat[-2]
+            else:
+                raise ValueError('Invalid method [{}]'.format(self.method))
+            y_pred = self.interp(x_hat, y_hat, x_new)
+            return {'y_pred': y_pred, 'x_hat': x_hat, 'y_hat': y_hat}
+
+
+if __name__ == "__main__":
+
+    # define constants
+    model_type = 'gru'
+    seqlen = 100
+    sample_rate = 10
+    num_samples = seqlen // sample_rate  # 25
+    bs = 32
+    num_batches = 20
+    max_value = 2
+
+    out_path = './visuals/interp_1d_exp_{}_samplerate{}.pkl'.format(model_type, sample_rate)
+    if not os.path.isdir(os.path.dirname(out_path)):
+        os.makedirs(os.path.dirname(out_path))
+
+    # define GT
+    chirp_fn = lambda x: torch.sin(2*3.14*((1./seqlen)* x + (1./(seqlen*40)*torch.pow(x, 2)))) * max_value
+    x_new = torch.arange(seqlen, dtype=torch.float).to(device)
+    x_sampled = x_new[0::sample_rate].to(device)
+    if x_sampled[-1] != seqlen-1:
+        x_sampled = torch.cat((x_sampled.to(device), torch.tensor([seqlen-1]).to(device)))
+    y_new = chirp_fn(x_new).to(device)
+
+    # define model
+    models ={
+        'no_sampling': Interpolator(model_type, 'no_sampling', seqlen, max_value, min_offset=-1, max_offset=1).to(device), # offsets ignored
+        'non_adaptive': Interpolator(model_type, 'non_adaptive', len(x_sampled), max_value, min_offset=-1, max_offset=1).to(device), # offsets ignored
+        'adaptive': Interpolator(model_type, 'adaptive', len(x_sampled), max_value, min_offset=-(sample_rate / 2), max_offset=(sample_rate / 2)).to(device),
+        'super_adaptive': Interpolator(model_type, 'super_adaptive', len(x_sampled), max_value, min_offset=10e-3, max_offset=2*sample_rate).to(device),
+    }
+    opts = {name: torch.optim.SGD(model.parameters(), lr=0.001, momentum=0.9) for name, model in models.items()}
+    loss = torch.nn.MSELoss().to(device)
+    n_params = {name: sum(p.numel() for p in model.parameters()) for name, model in models.items()}
+
+    print('#Parameters:')
+    for name, p in n_params.items():
+        print('{}: {}'.format(name, p))
+
+    tensor2numpy = lambda t: t.detach().cpu().numpy()
+    dict2numpy = lambda d: {k: tensor2numpy(v) for k, v in d.items()}
+    ddict2numpy = lambda dd: {k: dict2numpy(d) for k, d in dd.items()}
+
+    buffer = {
+        'sample_rate': sample_rate,
+        'model_type': model_type,
+        'model_names': list(models.keys()),
+        'n_params': n_params,
+        'x_new': tensor2numpy(x_new),
+        'y_new': tensor2numpy(y_new),
+        'x_sampled': tensor2numpy(x_sampled),
+        'train': [],
+
+    }
+
+    for step_i in tqdm(range(10000)):
+        preds = {name: model(x_new, x_sampled) for name, model in models.items()}
+        errs = {name: loss(preds[name]['y_pred'], y_new) for name in models.keys()}
+
+        for name in models.keys():
+            opts[name].zero_grad()
+            errs[name].backward()
+            opts[name].step()
+
+        # buffer training state
+        if step_i % 100 == 0:
+            # noinspection PyTypeChecker
+            buffer['train'].append({
+                'step': step_i,
+                'preds': ddict2numpy(preds),
+                'errs': dict2numpy(errs),
+            })
+
+    # Save pkl
+    with open(out_path, 'wb') as handle:
+        pickle.dump(buffer, handle, protocol=pickle.HIGHEST_PROTOCOL)
--- torchinterp_vis.py
+++ torchinterp_vis.py
@@ -1,125 +1,39 @@
+import pickle
 import torch
-import matplotlib.pyplot as plt
-import time
-import numpy as np
-from torchinterp1d import Interp1d
 import os
 import shutil
 import cv2
 from tqdm import tqdm
+import matplotlib.pyplot as plt
+import numpy as np
 
-class ToyModel(torch.nn.Module):
-    def __init__(self, _min, _max, width):
-        super(ToyModel, self).__init__()
-        assert _min < _max
-        self.min = _min
-        self.max = _max
-        self.lin1 = torch.nn.Linear(width, width)
-        self.relu = torch.nn.ReLU(inplace=True)
-        self.lin2 = torch.nn.Linear(width, width)
-        self.sig = lambda x: torch.sigmoid(x)*(self.max-self.min) + self.min  # (-1, 1)
-
-    def forward(self, x):
-        out = self.lin1(x)
-        out = self.relu(out)
-        out = self.lin2(out)
-        out = self.sig(out)
-        return out
-
-class Interpolator(torch.nn.Module):
-    def __init__(self, method, n_samples, amp, min_offset, max_offset):
-        super(Interpolator, self).__init__()
-        self.method = method
-        self.n_samples = n_samples
-        self.offset_model = ToyModel(min_offset, max_offset, width=n_samples-2)
-        self.value_model = ToyModel(-amp, amp, width=n_samples)
-        self.input_noise = torch.rand(n_samples)
-        self.interp = Interp1d()
-
-    def forward(self, x_new, x_sampled):
-        if self.method == 'no_sampling':
-            return {'y_pred': self.value_model(self.input_noise)}
-        else:
-            y_hat = self.value_model(self.input_noise)
-            offsets = self.offset_model(self.input_noise[:-2])
-            offsets = torch.cat((torch.tensor([0.]), offsets, torch.tensor([0.])))
-            if self.method == 'non_adaptive':
-                x_hat = x_sampled
-            elif self.method == 'adaptive':
-                x_hat = x_sampled + offsets
-            elif self.method == 'super_adaptive':
-                offsets[-2] /= 2.  # FIXME - ugly hack for super-adaptive case (avoiding the assertion)
-                x_hat = torch.zeros_like(y_hat)
-                for i in range(1, len(x_sampled) - 1):
-                    x_hat[i] = x_hat[i - 1] + offsets[i]
-                x_hat[-1] = seqlen - 1
-                assert x_hat[-1] > x_hat[-2]
-            else:
-                raise ValueError('Invalid method [{}]'.format(self.method))
-            y_pred = self.interp(x_hat, y_hat, x_new)
-            return {'y_pred': y_pred, 'x_hat': x_hat, 'y_hat': y_hat}
-
-
-if __name__ == "__main__":
-
-    # define constants
-    seqlen = 100
-    sample_rate = 4
-    num_samples = seqlen // sample_rate  # 25
-    bs = 32
-    num_batches = 20
-    max_value = 2
-
+if __name__ == '__main__':
+    input_pkl = '../visuals/interp/interp_1d_exp_mlp_samplerate4.pkl'
+    out_mp4 = input_pkl.replace('.pkl', '.mp4')
     plot_dir = '/tmp/interp_1d_exp_plt'
-    out_path = '../visuals/interp/interp_1d_exp_samplerate{}.mp4'.format(sample_rate)
     if os.path.isdir(plot_dir):
         shutil.rmtree(plot_dir)
     os.makedirs(plot_dir)
 
+    with open(input_pkl, 'rb') as handle:
+        buffer = pickle.load(handle)
 
-    # define GT
-    chirp_fn = lambda x: torch.sin(2*3.14*((1./seqlen)* x + (1./(seqlen*40)*torch.pow(x, 2)))) * max_value
-    x_new = torch.arange(seqlen, dtype=torch.float)
-    x_sampled = x_new[0::sample_rate]
-    if x_sampled[-1] != seqlen-1:
-        x_sampled = torch.cat((x_sampled, torch.tensor([seqlen-1])))
-    y_new = chirp_fn(x_new)
-
-    # define model
-    models ={
-        'no_sampling': Interpolator('no_sampling', seqlen, max_value, min_offset=-1, max_offset=1), # offsets ignored
-        'non_adaptive': Interpolator('non_adaptive', len(x_sampled), max_value, min_offset=-1, max_offset=1), # offsets ignored
-        'adaptive': Interpolator('adaptive', len(x_sampled), max_value, min_offset=-(sample_rate / 2), max_offset=(sample_rate / 2)),
-        'super_adaptive': Interpolator('super_adaptive', len(x_sampled), max_value, min_offset=10e-3, max_offset=2*sample_rate),
-    }
-    opts = {name: torch.optim.SGD(model.parameters(), lr=0.001, momentum=0.9) for name, model in models.items()}
-    loss = torch.nn.MSELoss()
-
-    for step_i in tqdm(range(1000)):
-        preds = {name: model(x_new, x_sampled) for name, model in models.items()}
-        errs = {name: loss(preds[name]['y_pred'], y_new) for name in models.keys()}
-
-        for name in models.keys():
-            opts[name].zero_grad()
-            errs[name].backward()
-            opts[name].step()
-
+    for snapshot in tqdm(buffer['train']):
         # plot one frame
-        if step_i % 10 == 0:
-            fig, axs = plt.subplots(1, len(models.keys()), figsize=(24, 6))
-            fig.suptitle('Sample rate [{}] | Step [{}]'.format(sample_rate, step_i))
-
-            for ax_i, name in enumerate(models.keys()):
-                axs[ax_i].set_title(name + ' - MSE[{:.4f}]'.format(errs[name].detach()))
-                axs[ax_i].plot(x_new, y_new, label='GT signal')
+        if snapshot['step'] % 10 == 0:
+            fig, axs = plt.subplots(1, len(buffer['model_names']), figsize=(24, 6))
+            fig.suptitle('{} | Sample rate [{}] | Step [{}]'.format(buffer['model_type'], buffer['sample_rate'], snapshot['step']))
+    
+            for ax_i, name in enumerate(buffer['model_names']):
+                axs[ax_i].set_title(name + '- #Params[{}] - MSE[{:.4f}]'.format(buffer['n_params'][name], snapshot['errs'][name]))
+                axs[ax_i].plot(buffer['x_new'], buffer['y_new'], label='GT signal')
                 if name == 'no_sampling':
-                    axs[ax_i].plot(x_new, preds[name]['y_pred'].detach(), linestyle='-', marker='o', color='green')
+                    axs[ax_i].plot(buffer['x_new'], snapshot['preds'][name]['y_pred'], linestyle='-', marker='o', color='green')
                 else:
-                    axs[ax_i].plot(x_sampled, y_new[x_sampled.to(torch.long)], linestyle='None', marker='v')
-                    axs[ax_i].plot(preds[name]['x_hat'].detach(), preds[name]['y_hat'].detach(), linestyle='-', marker='o')
-            plt.savefig(os.path.join(plot_dir, 'step_{:05d}.png'.format(step_i)))
+                    axs[ax_i].plot(buffer['x_sampled'], buffer['y_new'][buffer['x_sampled'].astype(int)], linestyle='None', marker='v')
+                    axs[ax_i].plot(snapshot['preds'][name]['x_hat'], snapshot['preds'][name]['y_hat'], linestyle='-', marker='o')
+            plt.savefig(os.path.join(plot_dir, 'step_{:05d}.png'.format(snapshot['step'])))
             plt.close()
-
 
     # save clip
     images = [os.path.join(plot_dir, f) for f in os.listdir(plot_dir) if f.endswith('.png')]
@@ -127,7 +41,7 @@ if __name__ == "__main__":
     frame = cv2.imread(images[0])
     height, width, layers = frame.shape
     fourcc = cv2.VideoWriter_fourcc(*'MP4V')
-    video = cv2.VideoWriter(out_path, fourcc, 5., (width, height))
+    video = cv2.VideoWriter(out_mp4, fourcc, 5., (width, height))
     for image in tqdm(images):
         video.write(cv2.imread(image))
 
